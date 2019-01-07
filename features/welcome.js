e71298dc5bfe0b988e0e97b5176ff0c5e32dd03f
euro <phuchit@omise.co>
2019-01-07 15:53:44 +0700

    :fire: Remove welcome feature

--- features/welcome.js
+++ features/welcome.js
@@ -1,10 +1,5 @@
 const handle = member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
+  console.log(member)
 }
 
 module.exports.handle = handle
