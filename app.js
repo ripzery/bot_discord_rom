06befa8629e5a8e05312b2654479c71cd43393f9
euro <phuchit@omise.co>
2019-01-08 11:06:23 +0700

    :wrench: Add some response and listen to port 5000

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const Discord = require('discord.js');
+const http = require('http');
 const auth = require('./auth.json')
 const welcome = require('./features/welcome.js')
 const message = require('./features/message.js')
 const client = new Discord.Client();
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Welcome to forever blank ROM-UI Bot page!');
+  res.end();
+}).listen(5000);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
